27673a0d3b221b28ca384f21b739dc9174ab73d3
udaybhole <udaybhole3104@gmail.com>
2024-11-15 20:04:01 +0530

    first commit

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,27 @@
+export const Quote = ()=>{
+    return <div className="bg-slate-400 h-screen flex justify-center flex-col ">
+
+
+        <div className="flex justify-center "> 
+              <div className="  max-w-lg text-center text-4xl font-bold ">
+              Welcome To BuzzBliss.
+              <br />
+              Start Posting Your Own Blogs
+        </div>
+
+        </div>
+    
+
+        <div className="  mt-4 text-center font-semibold">
+                UDAY BHOLE
+        </div>
+
+        <div className="mt-1 text-center text-xs text-slate-500">
+            AIDS UNDERGRAD
+                
+        </div>
+
+        </div>
+      
+
+}
